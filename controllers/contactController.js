f2c1aff8ebec8683f96ce5bc30d41ebfd43b4d8d
MdTamimJD <tamimoli75285@gmail.com>
2024-10-13 00:08:44 +0600

    added frontend api

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -9,5 +9,5 @@ export const contactController = async (req, res) => {
         title,
         message
     })
-    res.status(200).redirect("http://127.0.0.1:5500/client/index.html#footerSection")
+    res.status(200).redirect("https://jd-tamim.netlify.app/#contactSection")
 }
